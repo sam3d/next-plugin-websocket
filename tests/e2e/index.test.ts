61ef7eb05f608ff7c6fc1f8ca91a83300538293a
Sam Holmes <samholmes1337@gmail.com>
2023-02-13 16:40:08 +0000

    Add "npm pack" test helper

--- tests/e2e/index.test.ts
+++ tests/e2e/index.test.ts
@@ -1,6 +1,41 @@
 import execa from "execa";
 
-test("make a temporary directory", async () => {
-  const { stdout: tempDir } = await execa("mktemp", ["-d"]);
-  console.log(tempDir);
+type NpmPackOutput = {
+  id: string;
+  name: string;
+  version: string;
+  size: number;
+  unpackedSize: number;
+  shasum: string;
+  integrity: string;
+  filename: string;
+  files: NpmPackOutputFile[];
+  entryCount: number;
+  bundled: any[];
+};
+
+type NpmPackOutputFile = {
+  path: string;
+  size: number;
+  mode: number;
+};
+
+async function runNpmPack() {
+  const { stdout: outputDir } = await execa("mktemp", ["-d"]);
+
+  const { stdout } = await execa("npm", [
+    "pack",
+    "--json",
+    "--pack-destination",
+    outputDir,
+  ]);
+
+  const output = JSON.parse(stdout)[0] as NpmPackOutput;
+
+  return { outputDir, ...output };
+}
+
+test("Next.js 13.1 with Yarn and app directory", async () => {
+  const packOutput = await runNpmPack();
+  console.log(packOutput);
 });
