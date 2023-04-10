7efd963671015ef863fe9a938c3b4ab2bd7a67ca
yoieh <Yoieh@live.se>
2023-04-10 05:11:33 +0200

    Addes support for NetxtJS > 13.1.6
    fixed: #4 
    
    `ensureApiPage` was removed in v13.1.7-cannary.14
    
    https://github.com/vercel/next.js/pull/45716/files#diff-2f6fd5a9538abfaf3d5b76a82a8d1ffa029f84a36f0cdae5460520c5cfc2db6bL1402

--- src/index.ts
+++ src/index.ts
@@ -78,7 +78,11 @@ function hookNextNodeServer(this: NextNodeServer) {
     if (!isPageFound) return false;
 
     // Ensure that the page gets built, if it exists
-    await this.ensureApiPage(page);
+    if(this.ensureApiPage) {
+      await this.ensureApiPage(page);
+    } else {
+      await this.ensurePage({page, clientOnly: false});
+    }
 
     // Get the path of the built page. Will throw an error if the page doesn't
     // exist. This is fine to ignore, as it just falls into one of the many
